71cde4c48b4c7cc9d1e928a1890af2f2f5518568
D4rk-h4ck3r <sakesnudes@gmail.com>
2025-11-24 22:07:12 -0300

    Actualizar OsintWeb2.py

--- OsintWeb2.py
+++ OsintWeb2.py
@@ -1,43 +1,35 @@
-# Nombre del script: mi_script.py
-# Descripción: Osint web esta basado a abrir web de informacion de fuentes abiertas.
-# Autor: Hans Saldias
-# Fecha: 24 de noviembre de 2025
-
-import webbrowser
+# creado por Hans Saldias
+# 24/11/2025
+# script para buscar informacion en web de fuentes abiertas
+import os
 
 def mostrar_menu():
-    print("\033[33m=== Menú de Opciones de OSINT ===\n \033[32m== Creted by: D4rk-h4ck3r ==\033[0m")
-    print("\033[33m1. OSINT Framework")
+    print("=== Menú de Opciones de OSINT ===")
+    print("1. OSINT Framework")
     print("2. Maltego")
     print("3. Shodan")
     print("4. Censys")
     print("5. Recon-ng")
-    print("0. Exit")
+    print("6. Salir")
 
-def abrir_url(opcion):
-    urls = {
-        "1": "https://osintframework.com/",
-        "2": "https://www.paterva.com/web7/",
-        "3": "https://www.shodan.io/",
-        "4": "https://censys.io/",
-        "5": "https://github.com/lanmaster53/recon-ng"
-    }
-    
-    if opcion in urls:
-        webbrowser.open(urls[opcion])
+def abrir_herramienta(opcion):
+    if opcion == '1':
+        os.system("termux-open https://osintframework.com")
+    elif opcion == '2':
+        os.system("termux-open https://www.maltego.com")
+    elif opcion == '3':
+        os.system("termux-open https://www.shodan.io")
+    elif opcion == '4':
+        os.system("termux-open https://censys.io")
+    elif opcion == '5':
+        os.system("termux-open https://recon-ng.com")
+    elif opcion == '6':
+        print("Saliendo...")
+        exit()
     else:
-        print("Opción no válida. Por favor, elige una opción del menú.")
+        print("Opción no válida.")
 
-def main():
-    while True:
-        mostrar_menu()
-        opcion = input("Selecciona una opción (1-6): ")
-        
-        if opcion == "0":
-            print("Saliendo del programa...")
-            break
-        
-        abrir_url(opcion)
-
-if __name__ == "__main__":
-    main()
+while True:
+    mostrar_menu()
+    opcion = input("Selecciona una opción (1-6): ")
+    abrir_herramienta(opcion)
